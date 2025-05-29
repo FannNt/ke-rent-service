bbc74dc660ffa859edcc4fdce258019925d82ad0
restumi <maulanaismael5@gmail.com>
2025-05-29 14:39:59 +0700

    add login page

---
+++ resources/views/components/button.blade.php
@@ -0,0 +1,3 @@
+<button {{ $attributes->merge(['class' => 'btn btn-primary w-50']) }}>
+    {{ $slot }}
+</button>
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function(){
     return "Kerent API";
 });
+
+Route::get('/login', function(){
+    return view("login");
+})->name('login');
