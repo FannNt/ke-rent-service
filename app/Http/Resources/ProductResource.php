3c2a8e099c660c919f00912b5409be9242b72cb7
Fannn <affanhelmi111@gmail.com>
2025-06-01 16:51:05 +0700

    [+] add address text on ProductResource

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -22,6 +22,7 @@ class ProductResource extends JsonResource
             'category' => $this->category,
             'description' => $this->description,
             'additional_note' => $this->additional_note,
+            'address_text' => $this->address_text,
             'year' => $this->year,
             'latitude' => $this->latitude,
             'longitude' => $this->longitude,
