51b43b7a39c1a1a476860df95f60b8a3f99414fb
Fannn <affanhelmi111@gmail.com>
2025-06-02 09:07:45 +0700

    [~] change url field to image on ProductResource

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -27,7 +27,7 @@ class ProductResource extends JsonResource
             'latitude' => $this->latitude,
             'longitude' => $this->longitude,
             'is_available' => $this->is_available ?? 1,
-            'url' => $this->whenLoaded('image', function (){
+            'image' => $this->whenLoaded('image', function (){
                 return $this->image->pluck('image');
             })
 
